4d8f7268e0f0d380cabd485889f046b7ff3e4f25
zakumann <samuel742able@gmail.com>
2023-07-19 21:21:47 +0900

    07192023

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ClassPrototype from './R200/R012_Class&Prototype'
+import Props from './R200/R017_Props'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ClassPrototype/>
+      <Props props_val="THIS IS PROPS"/>
     </div>
   );
 }
